019416eb36ff2b8e3418c54d9b59e3443e6af2eb
Ashok Kumar Choppadandi <ashokkumar98778@gmail.com>
2020-05-27 15:42:43 +0530

    Update ReadIncrementalDataFromMongo.scala
    Reading Incremental Data (Delta) from MongoDB based on _id.oid

--- MongoSparkExamples/src/main/scala/com/bigdata/spark/mongodb/ReadIncrementalDataFromMongo.scala
+++ MongoSparkExamples/src/main/scala/com/bigdata/spark/mongodb/ReadIncrementalDataFromMongo.scala
@@ -30,34 +30,39 @@ object ReadIncrementalDataFromMongo {
 
     val spark = SparkSession
       .builder()
-      .appName("ReadDataFromMongo")
+      .appName("ReadIncrementalDataFromMongo")
       .master("local")
       .getOrCreate()
 
-    val timestampAsString = "Read From a File"//spark.read.textFile(lastUpdatedTimestampFilePath).first()
-    val lastRetrievedTimestamp = getLastRetrievedTimestamp(timestampAsString)
+    val timestampAsString = spark.read.textFile(lastUpdatedTimestampFilePath).first().trim
 
     val mongoDBURL = s"mongodb://${user}:${password}@${host}:${port}/"
     println("MongoDB URL :: " + mongoDBURL)
 
     val df = spark.read.format("mongo").option("uri", mongoDBURL).option("database", db).option("collection", collectionName).load()
-    val df2 = df.filter(col("submittedTime").>(lastRetrievedTimestamp))
 
-    df2.show()
+    val df2 = if(timestampAsString.length != 0 && timestampAsString != null && !timestampAsString.equals("null")) {
+      df.filter(col("_id.oid").>(timestampAsString))
+    } else {
+      df
+    }
+
+    df2.show(false)
     df2.printSchema()
 
-    df2.write.mode(SaveMode.Append).json(outputPath)
+    df2.write.mode(SaveMode.Overwrite).json(outputPath)
     println("Output Written Successfully...!!!")
 
-    val lastTimestampWritten = df2.select(max(col("submittedTime")))
-    // Write it to a file.
+    val lastTimestampWritten = df2.select(max(col("_id.oid")))
+    lastTimestampWritten.show(false)
+    lastTimestampWritten.coalesce(1).write.mode(SaveMode.Overwrite).text(lastUpdatedTimestampFilePath)
+    println("Last Read ObjectId is written successfully to file...!!!")
 
   }
 
-  def getLastRetrievedTimestamp(timestampAsString: String): Date = {
+  def getLastRetrievedTimestamp: String => Date = (timestampAsString: String) => {
     val format = "EEE MMM dd yyyy HH:mm:ss 'GMT'Z (z)"
     val dateFormat = new SimpleDateFormat(format)
-
     dateFormat.parse(timestampAsString)
   }
 }
